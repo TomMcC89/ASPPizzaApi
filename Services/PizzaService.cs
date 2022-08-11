da84a6b4197ea0d46d8d9d13343b27662135ee95
TomMcC89 <80389082+TomMcC89@users.noreply.github.com>
2022-08-11 09:47:21 +0100

    Update PizzaService.cs

--- Services/PizzaService.cs
+++ Services/PizzaService.cs
@@ -5,11 +5,12 @@ namespace ContosoPizza.Services;
 public static class PizzaService {
 
     static List<Pizza> Pizzas {get;}
-    static int NextId = 3;
+    static int NextId = 4;
     static PizzaService() {
             Pizzas = new List<Pizza>{
                 new Pizza{Id=1, Name = "Classic Italian", IsGlutenFree=false},
-                new Pizza{Id=2, Name = "Veggie", IsGlutenFree=true}
+                new Pizza{Id=2, Name = "Veggie", IsGlutenFree=true},
+                new Pizza{Id=3, Name = "Pepperoni", IsGlutenFree=false}
 
             };
     }
