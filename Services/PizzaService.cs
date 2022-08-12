e66c2b2f57f6cb715596ed1f5b8835b56dd3b5ad
Tom McCawley <tom@mccawley.tech>
2022-08-12 12:48:16 +0100

    Added Ham & Mushroom

--- Services/PizzaService.cs
+++ Services/PizzaService.cs
@@ -5,14 +5,14 @@ namespace ContosoPizza.Services;
 public static class PizzaService {
 
     static List<Pizza> Pizzas {get;}
-    static int NextId = 5;
+    static int NextId = 6;
     static PizzaService() {
             Pizzas = new List<Pizza>{
                 new Pizza{Id=1, Name = "Classic Italian", IsGlutenFree=false},
                 new Pizza{Id=2, Name = "Veggie", IsGlutenFree=true},
                 new Pizza{Id=3, Name = "Hawaiian", IsGlutenFree=false},
                 new Pizza{Id=4, Name = "Pepperoni", IsGlutenFree=false},
-
+                new Pizza{Id=5, Name = "Ham & Mushroom", IsGlutenFree=false}
             };
     }
 
